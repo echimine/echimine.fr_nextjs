045cfd085adc55738eadfa03f5bc7535626d3ba2
echimine <eliott.chiminello@gmail.com>
2025-12-14 14:09:24 +0100

    :memo: added global css

--- src/app/(site)/layout.tsx
+++ src/app/(site)/layout.tsx
@@ -1,6 +1,6 @@
 import Footer from '@/app/components/Footer/Footer';
 import Header from '@/components/commons/navigations/Header/Header';
-
+import '../globals.css';
 export default function SiteLayout({
   children,
 }: Readonly<{
